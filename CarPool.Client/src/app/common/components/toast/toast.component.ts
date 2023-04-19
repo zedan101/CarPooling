dc04c39e0370edf0a5ae44c15bb74859422688e8
zedan101 <nitishdubey888@gmail.com>
2023-04-19 21:39:47 +0530

    ...

--- CarPool.Client/src/app/common/components/toast/toast.component.ts
+++ CarPool.Client/src/app/common/components/toast/toast.component.ts
@@ -6,7 +6,7 @@ import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-toast',
-	templateUrl: './toast.Component.html',
+	templateUrl: './toast.component.html',
 	host: { class: 'toast-container position-fixed top-0 end-0 p-3', style: 'z-index: 1200' },
 })
 export class ToastComponent {
